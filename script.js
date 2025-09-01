c32c4f1c58f31d90ce4333541b1fdcc78c35fa5c
zeealetha7-hash <zeealetha7@gmail.com>
2025-09-01 16:15:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Tambahan efek misalnya saat halaman dimuat
+window.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.hero-text h2').style.opacity = '1';
+    document.querySelector('.hero-text h2').style.transform = 'translateY(0)';
+});
